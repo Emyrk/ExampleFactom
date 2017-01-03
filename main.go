3bdb15eed024f86135bbb5f76d176ad07b4e8507
Steven Masley <stevenmasley@gmail.com>
2017-01-02 23:51:05 -0500

    init

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/FactomProject/factom"
+)
+
+var _ = factom.AddressLength
+var _ = fmt.Sprintf("")
+
+var ESKey string = "Es3gZoQbNd2p2nDDRtULkUaneoSJY1WTCQ7LSyNqHWZ2UkttuS1o"
+var FSKey string = "Fs2DNirmGDtnAZGXqca3XHkukTNMxoMGFFQxJA3bAjJnKzzsZBMH"
+
+func main() {
+	factom.CommitEntry(e, ec)
+}
